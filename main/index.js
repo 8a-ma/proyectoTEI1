63c3ee93dce9b2fcf0320ac5b9ecc975f77dab7d
8a-ma <81239922+8a-ma@users.noreply.github.com>
2021-08-30 09:10:35 -0400

    Add files via upload

--- main/index.js
+++ main/index.js
@@ -1,15 +1,4 @@
-document.querySelector('#btn1');
-
-function problema1() {
-  fetch('comunas.json')
-    .then(res => res.json())
-    .then(datos => {
-      pintarp1(datos);
-    })
-
-}
-
-function pintarp1(datos) {
+function problema1(datos) {
   for(let valores of datos){
     if(valores.problema1 <= 20){
       document.getElementById(valores.nombre).style.fill = '#cc0b00';
@@ -25,6 +14,74 @@ function pintarp1(datos) {
     }
     else if(80 < valores.problema1){
       document.getElementById(valores.nombre).style.fill = '#44a800';
+    }
+  }
+}
+
+function problema2(datos) {
+  for(let valores of datos){
+    if(valores.problema2 <= 20){
+      document.getElementById(valores.nombre).style.fill = '#cc0b00';
+    }
+    else if(20 < valores.problema2 && valores.problema2 <= 40){
+      document.getElementById(valores.nombre).style.fill = '#f04e00';
+    }
+    else if(40 < valores.problema2 && valores.problema2 <= 60){
+      document.getElementById(valores.nombre).style.fill = '#e6d201';
+    }
+    else if(60 < valores.problema2 && valores.problema2 <= 80){
+      document.getElementById(valores.nombre).style.fill = '#00a824';
+    }
+    else if(80 < valores.problema2){
+      document.getElementById(valores.nombre).style.fill = '#44a800';
+    }
+  }
+}
+
+function problema3(datos) {
+  for(let valores of datos){
+    if(valores.problema3 <= 20){
+      document.getElementById(valores.nombre).style.fill = '#cc0b00';
+    }
+    else if(20 < valores.problema3 && valores.problema3 <= 40){
+      document.getElementById(valores.nombre).style.fill = '#f04e00';
+    }
+    else if(40 < valores.problema3 && valores.problema3 <= 60){
+      document.getElementById(valores.nombre).style.fill = '#e6d201';
+    }
+    else if(60 < valores.problema3 && valores.problema3 <= 80){
+      document.getElementById(valores.nombre).style.fill = '#00a824';
+    }
+    else if(80 < valores.problema3){
+      document.getElementById(valores.nombre).style.fill = '#44a800';
+    }
+  }
+}
+
+
+var botones = document.querySelectorAll("button");
+for(i=0; i < botones.length; i++){
+  botones[i].onclick = function(){
+    if(this.id == "problema1"){
+      fetch('comunas.json')
+        .then(res => res.json())
+        .then(datos => {
+          window[this.id](datos);
+        })
+    }
+    else if(this.id == "problema2"){
+      fetch('comunas.json')
+        .then(res => res.json())
+        .then(datos => {
+            window[this.id](datos);
+        })
+    }
+    else if(this.id = "problema3"){
+      fetch('comunas.json')
+        .then(res => res.json())
+        .then(datos => {
+            window[this.id](datos);
+        })
     }
   }
 }
