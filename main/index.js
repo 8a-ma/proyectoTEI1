edee4d3de82fb74712d9c20bee88d6dbebe3b342
8a-ma <81239922+8a-ma@users.noreply.github.com>
2021-09-06 19:32:33 -0230

    Add files via upload

--- main/index.js
+++ main/index.js
@@ -19,84 +19,45 @@ function creadorTabla(json, key){
   }
 }
 
-function problema1(datos) {
+function coloreoServicios(datos, servicio_id) {
   for(let valores of datos){
-    if(valores.problema1 <= 20){
-      document.getElementById(valores.nombre).style.fill = '#cc0b00';
+    if( 0 < valores[servicio_id] && valores[servicio_id] <= 20){
+      document.getElementById(valores.nombre).style.fill = '#FF0000';
     }
-    else if(20 < valores.problema1 && valores.problema1 <= 40){
-      document.getElementById(valores.nombre).style.fill = '#f04e00';
+    else if(20 < valores[servicio_id] && valores[servicio_id] <= 40){
+      document.getElementById(valores.nombre).style.fill = '#FF6200';
     }
-    else if(40 < valores.problema1 && valores.problema1 <= 60){
-      document.getElementById(valores.nombre).style.fill = '#e6d201';
+    else if(40 < valores[servicio_id] && valores[servicio_id] <= 60){
+      document.getElementById(valores.nombre).style.fill = '#FFAD00';
     }
-    else if(60 < valores.problema1 && valores.problema1 <= 80){
-      document.getElementById(valores.nombre).style.fill = '#00a824';
+    else if(60 < valores[servicio_id] && valores[servicio_id] <= 80){
+      document.getElementById(valores.nombre).style.fill = '#FFFB00';
     }
-    else if(80 < valores.problema1){
-      document.getElementById(valores.nombre).style.fill = '#44a800';
+    else if(80 < valores[servicio_id]){
+      document.getElementById(valores.nombre).style.fill = '#04FF00';
+    }
+    else if(valores[servicio_id] == 0){
+      document.getElementById(valores.nombre).style.fill = '#BBCCA3';
     }
   }
-  ordenarMayoMenor(datos, 'problema1');
-  creadorTabla(datos, 'problema1');
+  ordenarMayoMenor(datos, servicio_id);
+  creadorTabla(datos, servicio_id);
 }
-
-function problema2(datos) {
-  for(let valores of datos){
-    if(valores.problema2 <= 20){
-      document.getElementById(valores.nombre).style.fill = '#cc0b00';
-    }
-    else if(20 < valores.problema2 && valores.problema2 <= 40){
-      document.getElementById(valores.nombre).style.fill = '#f04e00';
-    }
-    else if(40 < valores.problema2 && valores.problema2 <= 60){
-      document.getElementById(valores.nombre).style.fill = '#e6d201';
-    }
-    else if(60 < valores.problema2 && valores.problema2 <= 80){
-      document.getElementById(valores.nombre).style.fill = '#00a824';
-    }
-    else if(80 < valores.problema2){
-      document.getElementById(valores.nombre).style.fill = '#44a800';
-    }
-  }
-  ordenarMayoMenor(datos, 'problema2');
-  creadorTabla(datos, 'problema2');
-}
-
-function problema3(datos) {
-  for(let valores of datos){
-    if(valores.problema3 <= 20){
-      document.getElementById(valores.nombre).style.fill = '#cc0b00';
-    }
-    else if(20 < valores.problema3 && valores.problema3 <= 40){
-      document.getElementById(valores.nombre).style.fill = '#f04e00';
-    }
-    else if(40 < valores.problema3 && valores.problema3 <= 60){
-      document.getElementById(valores.nombre).style.fill = '#e6d201';
-    }
-    else if(60 < valores.problema3 && valores.problema3 <= 80){
-      document.getElementById(valores.nombre).style.fill = '#00a824';
-    }
-    else if(80 < valores.problema3){
-      document.getElementById(valores.nombre).style.fill = '#44a800';
-    }
-  }
-  ordenarMayoMenor(datos, 'problema3');
-  creadorTabla(datos, 'problema3');
-}
-
 
 var botones = document.querySelectorAll("button");
 for(i=0; i < botones.length; i++){
   botones[i].onclick = function(){
     switch (this.id) {
-      case "problema1":
-      case "problema2":
-      case "problema3":
+      case "IDSE":
+      case "CONDICIONES_LABORALES":
+      case "CONECTIVIDAD_Y_MOVILIDAD":
+      case "SALUD_Y_MEDIO_AMBIENTE":
+      case "VIVIENDA_Y_ENTORNO":
+      case "ICVU":
         fetch('comunas.json')
           .then(res => res.json())
           .then(datos => {
-            window[this.id](datos);
+            coloreoServicios(datos, this.id);
           })
         break;
     }
